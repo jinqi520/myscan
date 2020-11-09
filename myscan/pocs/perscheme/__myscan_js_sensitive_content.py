07ba67ddc77c9e4f5a982e9ac7e2988538ed8c4a
liuqingsong <1352220829@qq.com>
2020-11-09 17:54:07 +0800

    js中泄漏各类token的检测脚本

--- myscan/pocs/perscheme/__myscan_js_sensitive_content.py
+++ myscan/pocs/perscheme/__myscan_js_sensitive_content.py
@@ -17,50 +17,69 @@ class POC():
         self.dictdata = workdata.get("dictdata")  # python的dict数据，详情请看docs/开发指南Example dict数据示例
         # scheme的poc不同perfoler和perfile,没有workdata没有data字段,所以无self.url
         self.result = []  # 此result保存dict数据，dict需包含name,url,level,detail字段，detail字段值必须为dict。如下self.result.append代码
-        self.name = "js_sensitive_content"
-        self.vulmsg = "leak msg "
+        self.name = "js_sensitive_token"
+        self.vulmsg = "js leak token"
         self.level = 1  # 0:Low  1:Medium 2:High
 
     def verify(self):
         if self.dictdata.get("url").get("extension").lower() not in ["js"]:
             return
-        regx = [
-            # 匹配url
-            # r'(\b|\'|")(?:http:|https:)(?:[\w/\.]+)?(?:[a-zA-Z0-9_\-\.]{1,})\.(?:php|asp|ashx|jspx|aspx|jsp|json|action|html|txt|xml|do)(\b|\'|")',
-            # 匹配邮箱
-            # r'[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)+',
-            # 匹配token或者密码泄露
-            # 例如token = xxxxxxxx, 或者"apikey" : "xssss"
-            r'\b(?:secret|wxappid|wxsecret|qqmapkey|secret_key|token|secret_token|auth_token|access_token|username|password|aws_access_key_id|aws_secret_access_key|secretkey|authtoken|accesstoken|access-token|authkey|client_secret|bucket|email|HEROKU_API_KEY|SF_USERNAME|PT_TOKEN|id_dsa|clientsecret|client-secret|encryption-key|pass|encryption_key|encryptionkey|secretkey|secret-key|bearer|JEKYLL_GITHUB_TOKEN|HOMEBREW_GITHUB_API_TOKEN|api_key|api_secret_key|api-key|private_key|client_key|client_id|sshkey|ssh_key|ssh-key|privatekey|DB_USERNAME|oauth_token|irc_pass|dbpasswd|xoxa-2|xoxrprivate-key|private_key|consumer_key|consumer_secret|access_token_secret|SLACK_BOT_TOKEN|slack_api_token|api_token|ConsumerKey|ConsumerSecret|SESSION_TOKEN|session_key|session_secret|slack_token|slack_secret_token|bot_access_token|passwd|api|eid|sid|api_key|apikey|userid|user_id|user-id)["\s]*(?::|=|=:|=>)["\s]*[a-z0-9A-Z]{8,64}"?',
-            # 匹配IP地址
-            # r'\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b',
-            # 匹配云泄露
-            # r'[\w]+\.cloudfront\.net',
-            # r'[\w\-.]+\.appspot\.com',
-            # r'[\w\-.]*s3[\w\-.]*\.?amazonaws\.com\/?[\w\-.]*',
-            # r'([\w\-.]*\.?digitaloceanspaces\.com\/?[\w\-.]*)',
-            # r'(storage\.cloud\.google\.com\/[\w\-.]+)',
-            # r'([\w\-.]*\.?storage.googleapis.com\/?[\w\-.]*)',
-            # 匹配手机号
-            # r'(?:139|138|137|136|135|134|147|150|151|152|157|158|159|178|182|183|184|187|188|198|130|131|132|155|156|166|185|186|145|175|176|133|153|177|173|180|181|189|199|170|171)[0-9]{8}'
-            # 匹配域名
-            # r'((?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+(?:biz|cc|club|cn|com|co|edu|fun|group|info|ink|kim|link|live|ltd|mobi|net|online|org|pro|pub|red|ren|shop|site|store|tech|top|tv|vip|wang|wiki|work|xin|xyz|me))',
-        ]
+        regs = {
+            "Slack Token": "(xox[p|b|o|a]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})",
+            "RSA private key": "-----BEGIN RSA PRIVATE KEY-----",
+            "SSH (DSA) private key": "-----BEGIN DSA PRIVATE KEY-----",
+            "SSH (EC) private key": "-----BEGIN EC PRIVATE KEY-----",
+            "PGP private key block": "-----BEGIN PGP PRIVATE KEY BLOCK-----",
+            "Amazon AWS Access Key ID": "AKIA[0-9A-Z]{16}",
+            "Amazon MWS Auth Token": "amzn\\.mws\\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
+            "AWS API Key": "AKIA[0-9A-Z]{16}",
+            "Facebook Access Token": "EAACEdEose0cBA[0-9A-Za-z]+",
+            "Facebook OAuth": "[f|F][a|A][c|C][e|E][b|B][o|O][o|O][k|K].*['|\"][0-9a-f]{32}['|\"]",
+            "GitHub": "[g|G][i|I][t|T][h|H][u|U][b|B].*['|\"][0-9a-zA-Z]{35,40}['|\"]",
+            "Generic API Key": "[a|A][p|P][i|I][_]?[k|K][e|E][y|Y].*['|\"][0-9a-zA-Z]{32,45}['|\"]",
+            "Generic Secret": "[s|S][e|E][c|C][r|R][e|E][t|T].*['|\"][0-9a-zA-Z]{32,45}['|\"]",
+            "Google API Key": "AIza[0-9A-Za-z\\-_]{35}",
+            "Google Cloud Platform API Key": "AIza[0-9A-Za-z\\-_]{35}",
+            "Google Cloud Platform OAuth": "[0-9]+-[0-9A-Za-z_]{32}\\.apps\\.googleusercontent\\.com",
+            "Google Drive API Key": "AIza[0-9A-Za-z\\-_]{35}",
+            "Google Drive OAuth": "[0-9]+-[0-9A-Za-z_]{32}\\.apps\\.googleusercontent\\.com",
+            "Google (GCP) Service-account": "\"type\": \"service_account\"",
+            "Google Gmail API Key": "AIza[0-9A-Za-z\\-_]{35}",
+            "Google Gmail OAuth": "[0-9]+-[0-9A-Za-z_]{32}\\.apps\\.googleusercontent\\.com",
+            "Google OAuth Access Token": "ya29\\.[0-9A-Za-z\\-_]+",
+            "Google YouTube API Key": "AIza[0-9A-Za-z\\-_]{35}",
+            "Google YouTube OAuth": "[0-9]+-[0-9A-Za-z_]{32}\\.apps\\.googleusercontent\\.com",
+            "Heroku API Key": "[h|H][e|E][r|R][o|O][k|K][u|U].*[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}",
+            "MailChimp API Key": "[0-9a-f]{32}-us[0-9]{1,2}",
+            "Mailgun API Key": "key-[0-9a-zA-Z]{32}",
+            "Password in URL": "[a-zA-Z]{3,10}://[^/\\s:@]{3,20}:[^/\\s:@]{3,20}@.{1,100}[\"'\\s]",
+            "PayPal Braintree Access Token": "access_token\\$production\\$[0-9a-z]{16}\\$[0-9a-f]{32}",
+            "Picatic API Key": "sk_live_[0-9a-z]{32}",
+            "Slack Webhook": "https://hooks.slack.com/services/T[a-zA-Z0-9_]{8}/B[a-zA-Z0-9_]{8}/[a-zA-Z0-9_]{24}",
+            "Stripe API Key": "sk_live_[0-9a-zA-Z]{24}",
+            "Stripe Restricted API Key": "rk_live_[0-9a-zA-Z]{24}",
+            "Square Access Token": "sq0atp-[0-9A-Za-z\\-_]{22}",
+            "Square OAuth Secret": "sq0csp-[0-9A-Za-z\\-_]{43}",
+            "Twilio API Key": "SK[0-9a-fA-F]{32}",
+            "Twitter Access Token": "[t|T][w|W][i|I][t|T][t|T][e|E][r|R].*[1-9][0-9]+-[0-9a-zA-Z]{40}",
+            "Twitter OAuth": "[t|T][w|W][i|I][t|T][t|T][e|E][r|R].*['|\"][0-9a-zA-Z]{35,44}['|\"]"
+        }
         parser = dictdata_parser(self.dictdata)
-        for _ in regx:
-            texts = re.findall(_.encode(), parser.getresponsebody(), re.M | re.I)
+        for key in regs:
+            texts = re.findall(regs[key], parser.getresponsebody(), re.M | re.I)
             if texts:
-
-                vuls ="rule:{}\r\n".format(_)+"".join(["{}:{}\r\n".format(k, v.decode()) for k, v in enumerate(list(set(texts)))])
-                self.result.append({
-                    "name": self.name,
-                    "url": parser.getfilepath(),
-                    "level": self.level,  # 0:Low  1:Medium 2:High
-                    "detail": {
-                        "vulmsg": self.vulmsg,
-                        "text": vuls,
-                        "request": parser.getrequestraw(),
-                        "response": parser.getresponseraw(),
-                    }
-                })
-                return
+                texts = list(set(texts))
+                for result in texts:
+                    if len(result) < 100:
+                        msg = msg + "根据{}的正则表达式:{} 发现敏感信息:{} \n".format(key, regs[key], result)
+                        self.result.append({
+                            "name": self.name,
+                            "url": parser.getfilepath(),
+                            "level": self.level,  # 0:Low  1:Medium 2:High
+                            "detail": {
+                                "vulmsg": self.vulmsg,
+                                "text": msg,
+                                "request": parser.getrequestraw(),
+                                "response": parser.getresponseraw(),
+                            }
+                        })
